82f14a28d8eb0ec1b55793c5c3fd19f1f9f60b44
Gabriel Gomes <74213836+bieldade@users.noreply.github.com>
2024-05-13 19:03:30 -0300

    Update Exemplo.php

--- EstruturaDados/Vetor/Exemplo.php
+++ EstruturaDados/Vetor/Exemplo.php
@@ -9,7 +9,6 @@ $fruta4 = "morango";
 echo $fruta3;
 ?>
 
-
 <?php
 // Com o uso de vetores:
 // Neste exemplo vemos a mesma função com um numero menor de caracteres. Basta selecionar o numero (começando por zero)
@@ -18,4 +17,18 @@ echo $fruta3;
 $frutas = array("maçã", "banana", "laranja", "morango");
 
 echo $frutas[2]; 
+?>
+
+<?php
+// Usar vetor é como organizar uma sequencia de tipos especificos de cores numa fila de lapis.
+// Se existe 10 filas, cada fila tem uma cor e cada lapis na fila tem seu numero.
+// Então eu poderia simplesmente dizer:
+   $lapisvermelho = array("primeiro","segundo","terceiro", "quarto");
+   $lapisverde = array("primeiro","segundo","terceiro", "quarto");
+   $lapispreto = array("primeiro","segundo","terceiro", "quarto");
+
+   echo $lapisvermelho[0];
+// Na minhas 3 filas de lapis, eu selecionei o primeiro lapis da fila de lapis vermelho.
+// Organizar as filas foi trabalhoso mas agora eu posso simplesmente selecionar qual fila e qual lapis
+// de dentro da fila eu quero que apareça!   
 ?>
